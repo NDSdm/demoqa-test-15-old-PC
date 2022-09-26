ab54a511bc12653dc36ae1877d893b6d333f5186
N.D.S <iadimk0370@gmail.com>
2022-09-26 19:19:26 +0300

    add pageObjects

---
+++ src/test/java/pages/components/GeolocationComponent.java
@@ -0,0 +1,18 @@
+package pages.components;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class GeolocationComponent {
+
+    public GeolocationComponent setGeolocation(String state, String city) {
+        $("#state").click();
+        $("#state").$(byText(state)).click();
+        $("#city").click();
+        $("#city").$(byText(city)).click();
+
+        return this;
+
+    }
+
+}
